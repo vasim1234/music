fb3600b6f55cc395b4e831f3f53a587580c94141
vasim1234 <vasimaiyub@gmail.com>
2026-09-14 21:06:26 +0530

    Add Kotlin Android plugin to build.gradle.kts
    Added Kotlin Android plugin to build.gradle.kts.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("kotlin-android")  // ✅ Ye add karo
     id("dev.flutter.flutter-gradle-plugin")
 }
 
