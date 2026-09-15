8fdff83dfb263445626b17971b622965a0412f8d
vasim1234 <vasimaiyub@gmail.com>
2026-09-15 19:00:06 +0530

    Add flutter/foundation.dart import to audio_handler
    Added import for flutter/foundation.dart.

--- lib/services/audio_handler.dart
+++ lib/services/audio_handler.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:audio_service/audio_service.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:flutter/foundation.dart';  // ✅ Ye add karo
 
 class MyAudioHandler extends BaseAudioHandler with QueueHandler, SeekHandler {
   final AudioPlayer _player = AudioPlayer();
